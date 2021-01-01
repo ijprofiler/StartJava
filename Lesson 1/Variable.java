d21dfa2ecfb30a1b6b3ebbd4fff44184dae29c2a
Boris <ijprofiler@gmail.com>
2021-01-01 12:15:41 +0300

    implement HW_1

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args){
+        byte b = -1;
+        short sh = 22332;
+        int i = 43423423423;
+        long l = 345324523423;
+        float f = 1.4;
+        double d = -2.32;
+        char ch = 'B';
+        boolean bool = true;
+
+        System.out.println(b);
+        System.out.println(sh);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(bool);
+	}
+}
